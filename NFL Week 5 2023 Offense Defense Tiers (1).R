aa1d5068d2b09405e219265b3d777d00d93e3c45
IMK03 <116689104+IMK03@users.noreply.github.com>
2024-01-17 13:51:40 -0600

    Add files via upload

---
+++ NFL Week 5 2023 Offense Defense Tiers (1).R
@@ -0,0 +1,120 @@
+
+# Load R and Import necessary Libraries
+remove.packages("rlang")
+remove.packages("tidyverse")
+remove.packages("nflreadr")
+remove.packages("nflplotR")
+install.packages("rlang")
+install.packages("tidyverse")
+install.packages("nflreadr")
+install.packages("nflplotR")
+install.packages("ggplot2")
+#install.packages("nflfastr")
+
+library(tidyverse)
+library(nflreadr)
+library(nflplotR)
+
+
+
+# Loading in the Data and Taking a Look
+
+pbp_22 = load_pbp(2022)
+
+pbp_22
+colnames(pbp_22)
+View(pbp_22)
+
+
+pbp_22 |> 
+  filter(pass == 1) |> 
+  group_by(passer) |> 
+  summarise(ypa = mean(yards_gained, na.rm = TRUE),
+            plays = n()) |> 
+  arrange(-plays) |> 
+  filter(plays > 100) |> 
+  mutate(yards_gained = ypa * plays) |> 
+  select(passer, yards_gained)
+
+
+
+
+# RBs
+pbp_22 |> 
+  filter(posteam == "CAR", rush == 1) |> 
+  group_by(rusher) |> 
+  summarise(mean_epa = mean(epa),
+            success_rate = mean(success),
+            ypc = mean(yards_gained),
+            plays = n()) |> 
+  arrange(-mean_epa) |> 
+  filter(plays >= 20)
+
+
+
+
+
+
+# Offensive performances
+
+pbp_23 = load_pbp(2023)
+
+wk5_off = pbp_23 |> 
+  filter(week == 5, !is.na(yards_gained), (pass == 1 | rush == 1)) |> 
+  group_by(posteam) |> 
+  summarise(mean_epa = mean(epa),
+            success_rate = mean(success),
+            ypa = mean(yards_gained),
+            plays = n()) |> 
+  arrange(-mean_epa)
+wk5_off
+
+ggplot(wk5_off, aes(x = mean_epa, y = success_rate)) +
+  labs(
+    x = "EPA per play (explosiveness)",
+    y = "Success Rate (consistency)",
+    title = "NFL Offensive Performances (Week 5)",
+    caption = "By: Idrees Muhammad Kudaimi  |  Data @nflfastR"
+  ) +
+  stat_smooth(formula = y ~ x, method = "lm", geom = "line", se = FALSE, color = "gray") +
+  nflplotR::geom_mean_lines(aes(x0 = mean_epa, y0 = success_rate)) +
+  nflplotR::geom_nfl_logos(aes(team_abbr = posteam), width = .07, alpha = .7)
+
+ggsave("off_performance_w5.png", width = 16, height = 9, units = "cm")
+
+
+
+# Defensive Performances
+
+wk5_def = pbp_23 |> 
+  filter(week == 5, !is.na(yards_gained), (pass == 1 | rush == 1)) |> 
+  group_by(defteam) |> 
+  summarise(mean_epa = mean(epa),
+            success_rate = mean(success),
+            ypa = mean(yards_gained),
+            plays = n()) |> 
+  arrange(mean_epa)
+wk5_def
+
+
+# Final Plots
+
+ggplot(wk5_def, aes(x = mean_epa, y = success_rate)) +
+  labs(
+    x = "EPA per play Allowed (explosiveness)",
+    y = "Success Rate Allowed (consistency)",
+    title = "NFL Defensive Performances (Week 5)",
+    caption = "By: Idrees Muhammad Kudaimi  |  Data @nflfastR"
+  ) +
+  scale_x_reverse() +
+  scale_y_reverse() +
+  stat_smooth(formula = y ~ x, method = 'lm', geom = 'line', se = FALSE, color = "gray") +
+  nflplotR::geom_mean_lines(aes(x0 = mean_epa, y0 = success_rate)) +
+  nflplotR::geom_nfl_logos(aes(team_abbr = defteam), width = .07, alpha = .7)
+
+
+ggsave("def_performance_w5.png", width = 16, height = 9, units = "cm")
+
+
+
+
